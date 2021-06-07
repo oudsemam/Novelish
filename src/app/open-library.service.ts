e8800bae826660063ee6231ffbde3148ff95ee32
Mechial <mechial.harrison@gmail.com>
2021-06-07 19:58:48 -0400

    search by title added to service

--- src/app/open-library.service.ts
+++ src/app/open-library.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenLibraryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchTitle(searchTerm:string, type:string): Observable<any>{
+
+    //Searches based on title
+    // if (type == "title"){
+      let title = searchTerm.replace(/\s/g,'+')
+      return this.http.get(`http://openlibrary.org/search.json?title=${title}`)
+    // }
+  }
 }
