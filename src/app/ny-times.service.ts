382b38869d7b0b0549ed04f31b294d85d908f6bf
Mechial <mechial.harrison@gmail.com>
2021-06-07 21:32:59 -0400

    get NYT list added to service

--- src/app/ny-times.service.ts
+++ src/app/ny-times.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NyTimesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getList(listname:string): Observable<any>{
+    let list = listname.replace(/\s/g,'-')
+    return this.http.get(`https://api.nytimes.com/svc/books/v3/lists/current/${list}?api-key=2HQ1GkrWdYaq6CzEMgktkGYjAlqWQ5HC`)
+  }
 }
