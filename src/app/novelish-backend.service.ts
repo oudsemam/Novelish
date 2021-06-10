2005f8b502db307afa573b434f13388cdf341677
Mechial <mechial.harrison@gmail.com>
2021-06-09 20:36:37 -0400

    services to access most database endpoints created

--- src/app/novelish-backend.service.ts
+++ src/app/novelish-backend.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http'
+import { Observable, Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class NovelishBackendService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers():Observable<any>{
+    return this.http.get(`http://localhost:3000/users`)
+  }
+
+  getAllBooks():Observable<any>{
+    return this.http.get(`http://localhost:3000/books`)
+  }
+
+  getAllShelves():Observable<any>{
+    return this.http.get(`http://localhost:3000/shelves`)
+  }
+
+  getShelvesByUser(userId:number):Observable<any>{
+    return this.http.get(`http://localhost:3000/shelves/${userId}`)
+  }
+
+  getBooksFromShelf(shelf:string, userId:number):Observable<any>{
+    return this.http.get(`http://localhost:3000/shelves/${shelf}/${userId}`)
+  }
+
+  getAllNotes(userId:number):Observable<any>{
+    return this.http.get(`http://localhost:3000/notes/${userId}`)
+  }
+
+  getNotesByBook(userId:number, bookId:number):Observable<any>{
+    return this.http.get(`http://localhost:3000/notes/${userId}/${bookId}`)
+  }
+//HOw to declare body??
+  addNote(userId:number, bookId:number):void{
+     this.http.get(`http://localhost:3000/notes/${userId}/${bookId}`)
+}
+
+  deleteNote(userId:number, bookId:number):void{
+    this.http.delete(`http://localhost:3000/notes/${userId}/${bookId}`)
+  }
+
+  deleteShelf(userId:number, shelf:string):void{
+    this.http.delete(`http://localhost:3000/shelves/${shelf}/${userId}`)
+  }
+
+  removeBookFromShelf(shelf:string, userId:number, bookId:number){
+    this.http.delete(`http://localhost:3000/books/${shelf}/${userId}/${bookId}`)
+  }
+
+  removeUser(userId:number):void{
+    this.http.delete(`http://localhost:3000/users/${userId}`)
+  }
 }
