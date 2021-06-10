5ed2d40b690205fba2cc49a5cc7f1128306d75f6
Mechial <mechial.harrison@gmail.com>
2021-06-09 21:06:15 -0400

    services first pass complete

--- src/app/novelish-backend.service.ts
+++ src/app/novelish-backend.service.ts
@@ -35,24 +35,40 @@ export class NovelishBackendService {
   getNotesByBook(userId:number, bookId:number):Observable<any>{
     return this.http.get(`http://localhost:3000/notes/${userId}/${bookId}`)
   }
-//HOw to declare body??
-  addNote(userId:number, bookId:number):void{
-     this.http.get(`http://localhost:3000/notes/${userId}/${bookId}`)
+
+  addNote(userId:number, bookId:number, note:string):Observable<any>{
+     return this.http.post(`http://localhost:3000/notes/${userId}/${bookId}`, {note})
 }
 
-  deleteNote(userId:number, bookId:number):void{
-    this.http.delete(`http://localhost:3000/notes/${userId}/${bookId}`)
+  changeNote(userId:number, bookId:number, note:string):Observable<any>{
+  return this.http.put(`http://localhost:3000/notes/${userId}/${bookId}`, {note})
+}
+
+  addShelf(userId:number, shelf:string):Observable<any>{
+    return this.http.post(`http://localhost:3000/shelves/${userId}`, {shelf})
   }
 
-  deleteShelf(userId:number, shelf:string):void{
-    this.http.delete(`http://localhost:3000/shelves/${shelf}/${userId}`)
+  addUser(email:string):Observable<any>{
+    return this.http.post(`http://localhost:3000/users`, {email})
   }
 
-  removeBookFromShelf(shelf:string, userId:number, bookId:number){
-    this.http.delete(`http://localhost:3000/books/${shelf}/${userId}/${bookId}`)
+  addBookToShelf(book: {}, userId:number, shelf:string):Observable<any>{
+    return this.http.post(`http://localhost:3000/books/${userId}/${shelf}`, book)
   }
 
-  removeUser(userId:number):void{
-    this.http.delete(`http://localhost:3000/users/${userId}`)
+  deleteNote(userId:number, bookId:number):Observable<any>{
+    return this.http.delete(`http://localhost:3000/notes/${userId}/${bookId}`)
+  }
+
+  deleteShelf(userId:number, shelf:string):Observable<any>{
+    return  this.http.delete(`http://localhost:3000/shelves/${shelf}/${userId}`)
+  }
+
+  removeBookFromShelf(shelf:string, userId:number, bookId:number):Observable<any>{
+    return this.http.delete(`http://localhost:3000/books/${shelf}/${userId}/${bookId}`)
+  }
+
+  removeUser(userId:number):Observable<any>{
+    return this.http.delete(`http://localhost:3000/users/${userId}`)
   }
 }
