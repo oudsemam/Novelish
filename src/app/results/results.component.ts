8c4a433f1ef63649906f2e9b795c23c5f2d84845
Mechial <mechial.harrison@gmail.com>
2021-06-10 19:50:10 -0400

    result functions added

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -8,10 +8,21 @@ import { OpenLibraryService } from '../open-library.service';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
-
+  subscription: Subscription | null = null
   constructor(private OLService: OpenLibraryService) { }
 
+  resultList: [] | null = null
   ngOnInit(): void {
+    this.resultList = this.OLService.getResults()
+    this.subscription = this.OLService.getSubject().subscribe((subject)=>{
+      if(subject){
+        this.resultList = subject;
+      } else { this.resultList = []}
+    })
   }
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }}
 
 }
