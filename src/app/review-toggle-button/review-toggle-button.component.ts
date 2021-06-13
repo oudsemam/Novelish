1107e67e079351c9eb6ffb9675c167d5e0fbca33
Maggie Oudsema <maggieoudsema@gmail.com>
2021-06-13 13:04:43 -0400

    buttons kind of work

--- src/app/review-toggle-button/review-toggle-button.component.ts
+++ src/app/review-toggle-button/review-toggle-button.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faMinusCircle, faPlusCircle,  } from '@fortawesome/free-solid-svg-icons';
+import { faMinusCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { ReviewCat } from '../review-cat';
 
 @Component({
@@ -11,45 +11,81 @@ export class ReviewToggleButtonComponent implements OnInit {
   faPlusCircle = faPlusCircle;
   faMinusCircle = faMinusCircle;
 
+  proCategories: ReviewCat[] = [];
+  conCategories: ReviewCat[] = [];
+
   reviewCategories: ReviewCat[] = [
-      {
-        reviewCategory: 'Accuracy',
-      },
-      {
-        reviewCategory: 'Character Development',
-      },
-      {
-        reviewCategory: 'Editing',
-      },
-      {
-        reviewCategory: 'Informative',
-      },
-      {
-        reviewCategory: 'Organization',
-      },
-      {
-        reviewCategory: 'Pacing',
-      },
-      {
-        reviewCategory: 'Plot Development',
-      },
-      {
-        reviewCategory: 'Readability',
-      },
-      {
-        reviewCategory: 'World-Building',
-      },
-      {
-        reviewCategory: 'Worth the Time/Cost',
-      },
-      {
-        reviewCategory: 'Writing Style',
-      }
-    ];
+    {
+      reviewCategory: 'Accuracy',
+    },
+    {
+      reviewCategory: 'Character Development',
+    },
+    {
+      reviewCategory: 'Editing',
+    },
+    {
+      reviewCategory: 'Informative',
+    },
+    {
+      reviewCategory: 'Organization',
+    },
+    {
+      reviewCategory: 'Pacing',
+    },
+    {
+      reviewCategory: 'Plot Development',
+    },
+    {
+      reviewCategory: 'Readability',
+    },
+    {
+      reviewCategory: 'World-Building',
+    },
+    {
+      reviewCategory: 'Worth the Time/Cost',
+    },
+    {
+      reviewCategory: 'Writing Style',
+    },
+  ];
 
   constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+  togglePossitive(item: ReviewCat, i: number) {
+    if (
+      this.proCategories.some(
+        (proCategories) => proCategories.reviewCategory === item.reviewCategory
+      )
+    ) {
+      this.proCategories.splice(i, 1);
+    } else {
+      this.proCategories.push(item);
+    }
+  }
+
+  toggleNegative(item: ReviewCat, i: number) {
+    if (
+      this.conCategories.some(
+        (conCategories) => conCategories.reviewCategory === item.reviewCategory
+      )
+    ) {
+      this.conCategories.splice(i, 1);
+    } else {
+      this.conCategories.push(item);
+    }
+  }
+
+  isSelectedPossitive(item: ReviewCat) {
+    return this.proCategories.some(
+      (proCategories) => proCategories.reviewCategory === item.reviewCategory
+    );
+  }
+
+  isSelectedNegative(item: ReviewCat) {
+    return this.conCategories.some(
+      (conCategories) => conCategories.reviewCategory === item.reviewCategory
+    );
   }
 }
