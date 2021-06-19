fc66d37c7e1b844c5ac3d1c9f65a0f215f761f31
Kate Mora Woods <katemorawoods@gmail.com>
2021-06-19 17:31:39 -0400

    created true sidebar navigation. doesn't work on mobile

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import {
   faBook,
   faUserCircle,
   faBars,
   faStarHalfAlt
 } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
@@ -18,8 +20,18 @@ export class MainHeaderComponent implements OnInit {
   menuState: string = 'in';
   faStarHalfAlt=faStarHalfAlt;
 
-  constructor() {}
+  menuToggle: boolean = false;
+
+  constructor(public authService: AuthService,
+    public router: Router,
+    public ngZone: NgZone) {}
 
   ngOnInit(): void {}
 
+  hideShowMenu = () => {
+    this.menuToggle = !this.menuToggle;
+  }
+
 }
+
+
