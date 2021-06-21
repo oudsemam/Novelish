c2e3eb5c2ed55801d8988f30204a5511f8f54879
Maggie Oudsema <maggieoudsema@gmail.com>
2021-06-21 19:44:29 -0400

    added this back

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: 'AIzaSyASFRSJgq2MZm12I4Nxn7c8kXFtyc5zN9o',
+    authDomain: 'novel-ish-app.firebaseapp.com',
+    projectId: 'novel-ish-app',
+    storageBucket: 'novel-ish-app.appspot.com',
+    messagingSenderId: '153734898826',
+    appId: '1:153734898826:web:812837a286cb826e4bf973',
+    measurementId: 'G-QMSDK2S8W3',
+  },
+};
